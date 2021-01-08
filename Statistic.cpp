5cbc9c0651de06c17e283941752596681a0254d1
Rob Tillaart <rob.tillaart@gmail.com>
2021-01-08 15:14:13 +0100

    Arduino ci (#3)
    * add Arduino-ci
    * add unit test

--- Statistic.cpp
+++ Statistic.cpp
@@ -2,7 +2,7 @@
 //    FILE: Statistic.cpp
 //  AUTHOR: Rob dot Tillaart at gmail dot com
 //          modified at 0.3 by Gil Ross at physics dot org
-// VERSION: 0.4.1
+// VERSION: 0.4.2
 // PURPOSE: Recursive statistical library for Arduino
 //
 // NOTE: 2011-01-07 Gill Ross
@@ -28,39 +28,42 @@
 // unbiassed SE.
 // -------------
 //
-// HISTORY:
-// 0.1     2010-10-29 initial version
-// 0.2     2010-10-29 stripped to minimal functionality
-// 0.2.01  2010-10-30
-//         added minimim, maximum, unbiased stdev,
-//         changed counter to long -> int overflows @32K samples
-// 0.3     2011-01-07
-//         branched from 0.2.01 version of Rob Tillaart's code
-// 0.3.1   2012-11-10 minor edits
-// 0.3.2   2012-11-10 minor edits
-//         changed count -> unsigned long allows for 2^32 samples
-//         added variance()
-// 0.3.3   2015-03-07
-//         float -> double to support ARM (compiles)
-//          moved count() sum() min() max() to .h; for optimizing compiler
-// 0.3.4   2017-07-31
-//         Refactored const in many places
-//         [reverted] double to float on request as float is 99.99% of the cases
-//         good enough and float(32 bit) is supported in HW for some processors.
-// 0.3.5   2017-09-27
-//         Added #include <Arduino.h> to fix uint32_t bug
-// 0.4.0   2020-05-13
-//         refactor
-//         Added flag to switch on the use of stdDev runtime. [idea marc.recksiedl]
-// 0.4.1   2020-06-19 fix library.json
-//
+//  HISTORY:
+//  0.1     2010-10-29 initial version
+//  0.2     2010-10-29 stripped to minimal functionality
+//  0.2.01  2010-10-30
+//          added minimim, maximum, unbiased stdev,
+//          changed counter to long -> int overflows @32K samples
+//  0.3     2011-01-07
+//          branched from 0.2.01 version of Rob Tillaart's code
+//  0.3.1   2012-11-10 minor edits
+//  0.3.2   2012-11-10 minor edits
+//          changed count -> unsigned long allows for 2^32 samples
+//          added variance()
+//  0.3.3   2015-03-07
+//          float -> double to support ARM (compiles)
+//           moved count() sum() min() max() to .h; for optimizing compiler
+//  0.3.4   2017-07-31
+//          Refactored const in many places
+//          [reverted] double to float on request as float is 99.99% of the cases
+//          good enough and float(32 bit) is supported in HW for some processors.
+//  0.3.5   2017-09-27
+//          Added #include <Arduino.h> to fix uint32_t bug
+//  0.4.0   2020-05-13
+//          refactor
+//          Added flag to switch on the use of stdDev runtime. [idea marc.recksiedl]
+//  0.4.1   2020-06-19  fix library.json
+//  0.4.2   2021-01-08  add Arduino-CI + unit tests
+
 
 #include "Statistic.h"
 
+
 Statistic::Statistic(bool useStdDev)
 {
-    clear(useStdDev);
+  clear(useStdDev);
 }
+
 
 void Statistic::clear(bool useStdDev)	// useStdDev default true.
 {
@@ -73,6 +76,7 @@ void Statistic::clear(bool useStdDev)	// useStdDev default true.
     // note not _ssq but sum of square differences
     // which is SUM(from i = 1 to N) of f(i)-_ave_N)**2
 }
+
 
 // adds a new value to the data-set
 void Statistic::add(const float value)
@@ -96,11 +100,12 @@ void Statistic::add(const float value)
         // ~10% faster but limits the amount of samples to 65K as _cnt*_cnt overflows
         // float _store = _sum - _cnt * value;
         // _ssqdif = _ssqdif + _store * _store / (_cnt*_cnt - _cnt);
-		//
-		// solution:  TODO verify
-		// _ssqdif = _ssqdif + (_store * _store / _cnt) / (_cnt - 1);
+        //
+        // solution:  TODO verify
+        // _ssqdif = _ssqdif + (_store * _store / _cnt) / (_cnt - 1);
     }
 }
+
 
 // returns the average of the data-set added sofar
 float Statistic::average() const
@@ -108,6 +113,7 @@ float Statistic::average() const
     if (_cnt == 0) return NAN; // prevent DIV0 error
     return _sum / _cnt;
 }
+
 
 // Population standard deviation = s = sqrt [ S ( Xi - µ )2 / N ]
 // http://www.suite101.com/content/how-is-standard-deviation-used-a99084
@@ -118,12 +124,14 @@ float Statistic::variance() const
     return _ssqdif / _cnt;
 }
 
+
 float Statistic::pop_stdev() const
 {
     if (!_useStdDev) return NAN;
     if (_cnt == 0) return NAN; // prevent DIV0 error
     return sqrt( _ssqdif / _cnt);
 }
+
 
 float Statistic::unbiased_stdev() const
 {
